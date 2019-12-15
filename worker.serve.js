d42b2b3816d4399f3c4b9451f4e065300e8280d6
gcoda <gcoding@gmail.com>
2019-12-15 18:20:53 +0200

    feat: bind kv namespace

--- worker.serve.js
+++ worker.serve.js
@@ -1,16 +1,18 @@
-const Cloudworker = require('@dollarshaveclub/cloudworker')
+const CloudWorker = require('@dollarshaveclub/cloudworker')
+const kv = require('@dollarshaveclub/cloudworker/lib/kv')
 const fs = require('fs')
 const { promisify } = require('util')
 const { DEV_WORKER_PORT = 4321 } = process.env
 
-const readFile = path =>
-  promisify(fs.readFile)(path, 'utf8')
+const readFile = path => promisify(fs.readFile)(path, 'utf8')
 
 readFile('./bundle/worker.js')
   .then(workerBody => {
-    const cw = new Cloudworker(workerBody, {
+    const cw = new CloudWorker(workerBody, {
       debug: true,
-      // bindings: { KV: WorkerKV },
+      bindings: {
+        SESSION_KV: new kv.KeyValueStore('./mock-kv-session.json'),
+      },
     })
     cw.listen(DEV_WORKER_PORT)
     console.log({ DEV_WORKER_PORT })
